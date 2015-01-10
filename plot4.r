4b1d8a6d50dac05ab4d3a0168aad2b11893c7c0c
Will Moir <wgmoir@gmail.com>
2015-01-10 12:52:59 -0500

    Misc minor updates

--- plot4.r
+++ plot4.r
@@ -1,20 +1,31 @@
 ## Exploratory Data Analysis - Course Project 1
-## Plot 2
+## Plot 4
 
-## Set working directory to my directory for this course
+## The first part of this code creates the directories, downloads, unzips,
+## reads, subsets and transforms the data, but only if each step has not
+## already been performed.
+## The second part creates the plot
+
+## create directories
 if (!file.exists("./datasciencecoursera")) {dir.create("./datasciencecoursera")}
-setwd("datasciencecoursera")
+if (!file.exists("./datasciencecoursera/ExData_Plotting1/")) {
+  dir.create("./datasciencecoursera/ExData_Plotting1/")
+}
 
 ## download and unzip file to datasciencecoursera/cleaning dir
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-if (!file.exists("household_power_consumption.txt")) {
-  download.file(fileUrl, destfile = "household_power.zip", method = "curl")
-  unzip(zipfile = "household_power.zip")
+if (!file.exists("./datasciencecoursera/household_power_consumption.txt")) {
+  download.file(fileUrl, destfile = "./datasciencecoursera/household_power.zip", method = "curl")
+  unzip(zipfile = "household_power.zip", exdir = "./datasciencecoursera/")
 }
 
 # read and subset data, if not already loaded
-if (!exists("subset")) {
-  full <- read.table("household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, na.strings=c("?"), sep=';')
+if (!is.data.frame(subset)) {
+  full <- read.table("./datasciencecoursera/household_power_consumption.txt", 
+                     header = TRUE, 
+                     stringsAsFactors = FALSE, 
+                     na.strings=c("?"), 
+                     sep=';')
   subset <- full[full$Date %in% c('1/2/2007','2/2/2007'),]
   rm(full)
   subset$datetime <- paste(subset$Date, subset$Time)
@@ -25,7 +36,7 @@ if (!exists("subset")) {
 
 ## Plot 4
 
-png(filename = "./ExData_Plotting1/plot4.png", width = 480, height = 480, units = "px")
+png(filename = "./datasciencecoursera//ExData_Plotting1/plot4.png", width = 480, height = 480, units = "px")
 par(mfrow = c(2,2))
 with(subset, {
   plot(Time, Global_active_power, 
